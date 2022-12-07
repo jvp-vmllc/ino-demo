f7fd0eed7bc43ef930a18414d48aa27217566252
jvp-vmllc <john.papa@visionmetering.com>
2022-12-07 20:20:51 +0800

    analog readings from A0

--- sketch.ino
+++ sketch.ino
@@ -1,8 +1,12 @@
+#define ANALOG_PIN A0
+int value;
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello World!");
 }
 
 void loop() {
-  
+  value = analogRead(ANALOG_ PIN);
+  Serial.println(value);
 }
