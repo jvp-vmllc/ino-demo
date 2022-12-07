71802d92742bdd1ebf36a8bda858053a216c1f88
jvp-vmllc <john.papa@visionmetering.com>
2022-12-07 20:22:26 +0800

    add UART readings

--- sketch.ino
+++ sketch.ino
@@ -1,12 +1,18 @@
 #define ANALOG_PIN A0
 int value;
+String data;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello World!");
 }
 
-void loop() {
+void loop() { 
+  if (Serial.available () > 0){
+    data = Serial.readStringUntil('\n');
+    Serial.println(data);
+  }
+  
   value = analogRead(ANALOG_ PIN);
   Serial.println(value);
 }
