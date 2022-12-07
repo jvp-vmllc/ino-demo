89a3ea6b5b1f8a9e8a273c7e5481a45f6f707c1e
jvp-vmllc <john.papa@visionmetering.com>
2022-12-07 23:16:13 +0800

    remove printing of daat 1 and 2, andd add them

--- sketch.ino
+++ sketch.ino
@@ -12,10 +12,15 @@ void loop() {
   if (Serial.available () > 0){
     data1 = Serial.readStringUntil(',');
     data2 = Serial.readStringUntil('\n');
-    Serial.println(data1);
-    Serial.println(data2);
+    Serial.println(addTwoNum);
   }
   
   value = analogRead(ANALOG_PIN);
   Serial.println(value);
+}
+
+String addTwoNum(String data1, String data2){
+  int add;
+  add = data1.toInt() + data2.toInt();
+  return add;
 }
